4f2cca6ffb4b467ef4f9f3151e317a177d23ff51
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-01-27 00:46:43 +0530

    Added basic template code and library

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/counter/counter";
+import AppProvider from "./providers/app_providers";
+import MuiMode from "./components/mui/mui_mode";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <AppProvider>
+    //   <MuiMode />
+    // </AppProvider>
+    <Counter />
   );
 }
 
