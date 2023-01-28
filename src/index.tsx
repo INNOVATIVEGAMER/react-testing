4618b89ddde3e7dacb328226bca5d0bcf7996746
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-01-28 16:18:57 +0530

    checking lint staged

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const name = "prasad";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
