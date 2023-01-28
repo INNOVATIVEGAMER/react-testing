29bef3d2e4a19f9b9b066049491b46ed3537a164
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-01-28 16:20:48 +0530

    removed unnecessary code after lint-staged success check

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const name = "prasad";
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
